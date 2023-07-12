3b0d27f82c561b04bcf901a29c784a4d6add11c6
bielgennaro <zirondigabriel@gmail.com>
2023-07-12 00:39:35 -0300

    feat: starting project w/ eslint

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
