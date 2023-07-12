760c6b2ab1f2f19badf7d51a85adec51bbf9bb77
bielgennaro <zirondigabriel@gmail.com>
2023-07-12 01:18:11 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { SafeAreaView, Text } from "react-native";
+import styled from "styled-components/native";
+
+const TextNew = styled.Text`
+font-size: 20px;
+color: black;
+`
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>App</Text>
+      <TextNew>TESTE</TextNew>
     </SafeAreaView>
   );
 };
